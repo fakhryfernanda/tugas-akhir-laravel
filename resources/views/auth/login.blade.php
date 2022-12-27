1098a527547d78a9e5737f5c96bdfd834bea3c39
fakhryfernanda <mff023@gmail.com>
2022-12-27 19:10:00 +0700

    Membuat halaman login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,9 @@
+@extends('index')
+
+@section('content')
+<section class="mb-20">
+    <div class="container w-[1200px] mx-auto">
+        Halaman login
+    </div>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RegistrationController;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/pendaftaran', [RegistrationController::class, 'index']);
+Route::get('/login', [AuthController::class, 'login']);
