7f78fc4ca2ee025c65109f209ba18f14769910fe
fakhryfernanda <mff023@gmail.com>
2022-12-27 16:51:41 +0700

    Membuat halaman beranda dan navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,9 +8,24 @@
 
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <header>
-        
+<body class="bg-sky-200">
+    <header class="bg-black/20 py-4">
+        <div class="w-[1200px] mx-auto container flex items-center">
+            <div class="mr-32 flex items-center gap-3">
+                <img src="images/logo-11.jpg" alt="" width="40" class="inline">
+                <span class="text-2xl font-bold text-white">SMAN 11 BEKASI</span>
+            </div>
+            <nav>
+                <ul class="flex gap-5">
+                    <li class="text-white">
+                        <a href="/">BERANDA</a>
+                    </li>
+                    <li class="text-white">
+                        <a href="/pendaftaran">PPDB</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     </header>
     @section('content')
 </body>
