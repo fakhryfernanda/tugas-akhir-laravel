8961ea0ed2a5015147b6b72fc046a6752ef5302a
fakhryfernanda <mff023@gmail.com>
2022-12-28 11:15:07 +0700

    Menambahkan data orang tua

--- resources/views/ppdb/formulir.blade.php
+++ resources/views/ppdb/formulir.blade.php
@@ -5,7 +5,7 @@
     <div class="container w-[1200px] mx-auto">
         <h1 class="text-3xl font-semibold mb-5">FORMULIR PENERIMAAN PESERTA DIDIK BARU TAHUN 2023/2024</h1>
         <div>
-            <div x-data="app()" x-cloak>
+            <div x-data="app()">
                 <div x-show.transition="step === 'complete'" class="p-10 text-center bg-white rounded-lg shadow">
                     <i class="fa-solid fa-circle-check text-[72px] text-green-500"></i>
                     <h2 class="text-2xl mb-4 text-gray-800 text-center font-bold">Pendaftaran Berhasil</h2>
@@ -25,7 +25,7 @@
                 <div x-show.transition="step != 'complete'" class="py-6 px-8 bg-white border">
                     <!-- Top Navigation -->
                     <div class="border-b-2 py-4 mb-12">
-                        <div class="uppercase text-xs font-bold text-gray-500 mb-1" x-text="`Step: ${step} of 3`"></div>
+                        <div class="uppercase text-xs font-bold text-gray-500 mb-1" x-text="`Step: ${step} of ${totalStep}`"></div>
                         <div class="flex justify-between">
                             <div class="flex-1">
                                 <div x-show="step === 1">
@@ -39,13 +39,17 @@
                                 <div x-show="step === 3">
                                     <div class="text-lg font-bold text-gray-700">Data Alamat</div>
                                 </div>
+
+                                <div x-show="step === 4">
+                                    <div class="text-lg font-bold text-gray-700">Data Orang Tua</div>
+                                </div>
                             </div>
 
                             <div class="flex items-center w-64">
                                 <div class="w-full bg-gray-200 rounded-full mr-2">
-                                    <div class="rounded-full bg-green-500 text-xs h-2 text-center text-white" :style="'width: '+ parseInt(step / 3 * 100) +'%'"></div>
+                                    <div class="rounded-full bg-green-500 text-xs h-2 text-center text-white" :style="'width: '+ parseInt(step / totalStep * 100) +'%'"></div>
                                 </div>
-                                <div class="text-xs w-10 text-gray-600" x-text="parseInt(step / 3 * 100) +'%'"></div>
+                                <div class="text-xs w-10 text-gray-600" x-text="parseInt(step / totalStep * 100) +'%'"></div>
                             </div>
                         </div>
                     </div>
@@ -157,6 +161,169 @@
                                 </div>
                             </div>
                         </div>
+                        <div x-show.transition.in="step === 4" class="flex flex-col gap-3">
+                            {{-- Ayah --}}
+                            <p class="text-lg font-semibold">Ayah</p>
+                            <div class="flex items-center">
+                                <label for="nama_ayah" class="block w-[800px]">Nama Ayah</label>
+                                <input type="text" id="nama_ayah" name="nama_ayah" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                            </div>
+                            <div class="flex items-center">
+                                <label for="tahun_lahir_ayah" class="block w-[800px]">Tahun Lahir Ayah</label>
+                                <input type="text" id="tahun_lahir_ayah" name="tahun_lahir_ayah" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                            </div>
+                            <div class="flex items-center">
+                                <label for="pendidikan_ayah" class="block w-[800px]">Pendidikan Ayah</label>
+                                <select name="pendidikan_ayah" id="pendidikan_ayah" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="tidak_sekolah">Tidak Sekolah</option>
+                                    <option value="sd">SD Sederajat</option>
+                                    <option value="smp">SMP Sederajat</option>
+                                    <option value="sma">SMA Sederajat</option>
+                                    <option value="d1">D1</option>
+                                    <option value="d2">D2</option>
+                                    <option value="d3">D3</option>
+                                    <option value="d4/s1">D4/S1</option>
+                                    <option value="s2">S2</option>
+                                    <option value="s3">S3</option>
+                                </select>
+                            </div>
+                            <div class="flex items-center">
+                                <label for="pekerjaan_ayah" class="block w-[800px]">Pekerjaan Ayah</label>
+                                <select name="pekerjaan_ayah" id="pekerjaan_ayah" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="tidak_bekerja">Tidak Bekerja</option>
+                                    <option value="nelayan">Nelayan</option>
+                                    <option value="petani">Petani</option>
+                                    <option value="peternak">Peternak</option>
+                                    <option value="pns">PNS/TNI/POLRI</option>
+                                    <option value="karyawa_swasta">Karyawan Swasta</option>
+                                    <option value="wiraswasta">Wiraswasta</option>
+                                    <option value="wirausaha">Wirausaha</option>
+                                    <option value="buruh">Buruh</option>
+                                    <option value="pensiunan">Pensiunan</option>
+                                </select>
+                            </div>
+                            <div class="flex items-center">
+                                <label for="penghasilan_bulanan_ayah" class="block w-[800px]">Penghasilan Bulanan Ayah</label>
+                                <select name="penghasilan_bulanan_ayah" id="penghasilan_bulanan_ayah" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="penghasilan_1">Kurang dari Rp 1.000.000</option>
+                                    <option value="penghasilan_2">Rp 1.000.000 - Rp 3.000.000</option>
+                                    <option value="penghasilan_3">Rp 3.000.000 - Rp 5.000.000</option>
+                                    <option value="penghasilan_4">Rp 5.000.000 - Rp 10.000.000</option>
+                                    <option value="penghasilan_5">Lebih dari Rp 10.000.000</option>
+                                </select>
+                            </div>
+
+                            {{-- Ibu --}}
+                            <p class="text-lg font-semibold mt-4">Ibu</p>
+                            <div class="flex items-center">
+                                <label for="nama_ibu" class="block w-[800px]">Nama Ibu</label>
+                                <input type="text" id="nama_ibu" name="nama_ibu" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                            </div>
+                            <div class="flex items-center">
+                                <label for="tahun_lahir_ibu" class="block w-[800px]">Tahun Lahir Ibu</label>
+                                <input type="text" id="tahun_lahir_ibu" name="tahun_lahir_ibu" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                            </div>
+                            <div class="flex items-center">
+                                <label for="pendidikan_ibu" class="block w-[800px]">Pendidikan Ibu</label>
+                                <select name="pendidikan_ibu" id="pendidikan_ibu" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="tidak_sekolah">Tidak Sekolah</option>
+                                    <option value="sd">SD Sederajat</option>
+                                    <option value="smp">SMP Sederajat</option>
+                                    <option value="sma">SMA Sederajat</option>
+                                    <option value="d1">D1</option>
+                                    <option value="d2">D2</option>
+                                    <option value="d3">D3</option>
+                                    <option value="d4/s1">D4/S1</option>
+                                    <option value="s2">S2</option>
+                                    <option value="s3">S3</option>
+                                </select>
+                            </div>
+                            <div class="flex items-center">
+                                <label for="pekerjaan_ibu" class="block w-[800px]">Pekerjaan Ibu</label>
+                                <select name="pekerjaan_ibu" id="pekerjaan_ibu" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="tidak_bekerja">Tidak Bekerja</option>
+                                    <option value="nelayan">Nelayan</option>
+                                    <option value="petani">Petani</option>
+                                    <option value="peternak">Peternak</option>
+                                    <option value="pns">PNS/TNI/POLRI</option>
+                                    <option value="karyawa_swasta">Karyawan Swasta</option>
+                                    <option value="wiraswasta">Wiraswasta</option>
+                                    <option value="wirausaha">Wirausaha</option>
+                                    <option value="buruh">Buruh</option>
+                                    <option value="pensiunan">Pensiunan</option>
+                                </select>
+                            </div>
+                            <div class="flex items-center">
+                                <label for="penghasilan_bulanan_ibu" class="block w-[800px]">Penghasilan Bulanan Ibu</label>
+                                <select name="penghasilan_bulanan_ibu" id="penghasilan_bulanan_ibu" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="penghasilan_1">Kurang dari Rp 1.000.000</option>
+                                    <option value="penghasilan_2">Rp 1.000.000 - Rp 3.000.000</option>
+                                    <option value="penghasilan_3">Rp 3.000.000 - Rp 5.000.000</option>
+                                    <option value="penghasilan_4">Rp 5.000.000 - Rp 10.000.000</option>
+                                    <option value="penghasilan_5">Lebih dari Rp 10.000.000</option>
+                                </select>
+                            </div>
+
+                            {{-- Wali --}}
+                            <p class="text-lg font-semibold mt-4">Wali</p>
+                            <div class="flex items-center">
+                                <label for="nama_wali" class="block w-[800px]">Nama Wali</label>
+                                <input type="text" id="nama_wali" name="nama_wali" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                            </div>
+                            <div class="flex items-center">
+                                <label for="tahun_lahir_wali" class="block w-[800px]">Tahun Lahir Wali</label>
+                                <input type="text" id="tahun_lahir_wali" name="tahun_lahir_wali" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                            </div>
+                            <div class="flex items-center">
+                                <label for="pendidikan_wali" class="block w-[800px]">Pendidikan Wali</label>
+                                <select name="pendidikan_wali" id="pendidikan_wali" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="tidak_sekolah">Tidak Sekolah</option>
+                                    <option value="sd">SD Sederajat</option>
+                                    <option value="smp">SMP Sederajat</option>
+                                    <option value="sma">SMA Sederajat</option>
+                                    <option value="d1">D1</option>
+                                    <option value="d2">D2</option>
+                                    <option value="d3">D3</option>
+                                    <option value="d4/s1">D4/S1</option>
+                                    <option value="s2">S2</option>
+                                    <option value="s3">S3</option>
+                                </select>
+                            </div>
+                            <div class="flex items-center">
+                                <label for="pekerjaan_wali" class="block w-[800px]">Pekerjaan Wali</label>
+                                <select name="pekerjaan_wali" id="pekerjaan_wali" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="tidak_bekerja">Tidak Bekerja</option>
+                                    <option value="nelayan">Nelayan</option>
+                                    <option value="petani">Petani</option>
+                                    <option value="peternak">Peternak</option>
+                                    <option value="pns">PNS/TNI/POLRI</option>
+                                    <option value="karyawa_swasta">Karyawan Swasta</option>
+                                    <option value="wiraswasta">Wiraswasta</option>
+                                    <option value="wirausaha">Wirausaha</option>
+                                    <option value="buruh">Buruh</option>
+                                    <option value="pensiunan">Pensiunan</option>
+                                </select>
+                            </div>
+                            <div class="flex items-center">
+                                <label for="penghasilan_bulanan_wali" class="block w-[800px]">Penghasilan Bulanan Wali</label>
+                                <select name="penghasilan_bulanan_wali" id="penghasilan_bulanan_wali" class="w-full px-2 py-1 border border-black/20 outline outline-[3.5px] outline-transparent focus:outline-sky-300">
+                                    <option>Pilih :</option>
+                                    <option value="penghasilan_1">Kurang dari Rp 1.000.000</option>
+                                    <option value="penghasilan_2">Rp 1.000.000 - Rp 3.000.000</option>
+                                    <option value="penghasilan_3">Rp 3.000.000 - Rp 5.000.000</option>
+                                    <option value="penghasilan_4">Rp 5.000.000 - Rp 10.000.000</option>
+                                    <option value="penghasilan_5">Lebih dari Rp 10.000.000</option>
+                                </select>
+                            </div>
+                        </div>
                     </form>
                 </div>
 
@@ -173,7 +340,7 @@
 
                         <div class="w-1/2 text-right">
                             <button
-                                x-show="step < 3"
+                                x-show="step < totalStep"
                                 @click="step++"
                                 class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium" 
                             >Selanjutnya</button>
@@ -181,7 +348,7 @@
                             <input
                                 type="submit"
                                 @click="step = 'complete'"
-                                x-show="step === 3"
+                                x-show="step === totalStep"
                                 class="w-32 focus:outline-none border border-transparent py-2 px-5 rounded-lg shadow-sm text-center text-white bg-blue-500 hover:bg-blue-600 font-medium"
                                 value="Simpan"
                             >
@@ -195,7 +362,8 @@
 <script>
     function app() {
         return {
-            step: 1, 
+            step: 4,
+            totalStep: 4
         }
 	}
 </script>
