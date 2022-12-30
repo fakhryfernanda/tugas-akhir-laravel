30ad70565bf89c29292defe2f6a9f3005e7a76a8
fakhryfernanda <mff023@gmail.com>
2022-12-30 10:45:07 +0700

    Script js di file terpisah

--- resources/views/livewire/admin/data-pendaftar.blade.php
+++ resources/views/livewire/admin/data-pendaftar.blade.php
@@ -13,49 +13,7 @@
     {{-- x-data="$store.pendaftar" --}}
     {{-- x-init="getDataPendaftar()" --}}
 >
-    <script>
-        document.addEventListener('alpine:init', () => {
-            
-            Alpine.store('pendaftar', {
-                data: null,
-
-                async sortByKey(array, key)
-                {
-                    return array.sort(async function(a, b)
-                    {
-                        var x = await a[key]; var y = b[key]
-                        return ((x < y) ? -1 : ((x > y) ? 1 : 0))
-                    })
-                },
-                
-                getDataPendaftar()
-                {
-                    fetch('http://127.0.0.1:8000/api/pendaftar/all')
-                        .then(async (response) => {
-                            this.data = await response.json()
-                            this.data = this.data.data
-                            // this.data = sortByKey(this.data, 'nama_lengkap')
-                    })
-                },
-
-                getMean(obj)
-                {
-                    var count = 0
-                    var sum = 0
-
-                    Object.keys(obj).forEach(key => {
-                        if (key != 'id_pendaftar') {
-                            count++
-                            sum += obj[key]
-                        }
-                    });
-
-                    mean = sum/count
-                    return mean
-                },
-            })
-        })
-    </script>
+    <script src="{{ URL::to('js/admin-dashboard.js') }}"></script>
 
     <div 
         x-data="$store.pendaftar"
