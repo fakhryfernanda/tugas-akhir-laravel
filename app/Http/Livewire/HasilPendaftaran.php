f658eac3e3a6a97d264da6d6b549c5d2e4eecf4f
fakhryfernanda <mff023@gmail.com>
2022-12-31 11:06:37 +0700

    Buat komponen livewire terpisah

---
+++ app/Http/Livewire/HasilPendaftaran.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class HasilPendaftaran extends Component
+{
+    public function render()
+    {
+        return view('livewire.hasil-pendaftaran');
+    }
+}
---
+++ app/Http/Livewire/InformasiPendaftaran.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class InformasiPendaftaran extends Component
+{
+    public function render()
+    {
+        return view('livewire.informasi-pendaftaran');
+    }
+}
