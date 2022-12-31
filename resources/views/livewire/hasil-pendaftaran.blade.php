16cc606c18b69e6fbfaaf5b92f57920a51ba68f4
fakhryfernanda <mff023@gmail.com>
2022-12-31 11:05:07 +0700

    Buat komponen livewire terpisah

---
+++ resources/views/livewire/hasil-pendaftaran.blade.php
@@ -0,0 +1,15 @@
+<section class="mb-20">
+
+    <div class="container w-[1200px] mx-auto">
+        <div class="p-4 shadow-md bg-white mb-6">
+            <h1 class="text-3xl font-semibold mb-4">
+                Hasil Seleksi
+            </h1>
+            <p>
+                Halaman ini berisi hasil seleksi Penerimaan Peserta Didik Baru SMAN 11 Bekasi Periode 2023 / 2024.
+            </p>
+        </div>
+        <livewire:login /> 
+        <livewire:registrasi />
+    </div>
+</section>
