b9be90b1535dffb370fb0e0443293271a3282492
fakhryfernanda <mff023@gmail.com>
2023-01-01 21:51:18 +0700

    (+) buat komponen terpisah untuk detail pendaftar

---
+++ app/Http/Livewire/HalamanPendaftaran/DetailPendaftar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\HalamanPendaftaran;
+
+use Livewire\Component;
+
+class DetailPendaftar extends Component
+{
+    public function render()
+    {
+        return view('livewire.halaman-pendaftaran.detail-pendaftar');
+    }
+}
