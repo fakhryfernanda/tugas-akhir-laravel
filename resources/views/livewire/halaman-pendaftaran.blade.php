20f8389d02d3864445ad16047edf42fe48b88030
fakhryfernanda <mff023@gmail.com>
2023-01-01 17:01:00 +0700

    (+) Tanda bahwa sudah mengisi formulir

--- resources/views/livewire/halaman-pendaftaran.blade.php
+++ resources/views/livewire/halaman-pendaftaran.blade.php
@@ -3,10 +3,19 @@
         <div class="container w-[1200px] mx-auto">
             {{-- Fungsi livewire prevent gak bisa jalan kalo pake tag template --}}
             {{-- <template x-if="login.isLoggedIn"> --}}
-            @livewire('halaman-pendaftaran.formulir')
+                <div class="p-4 shadow-md bg-white mb-6">
+                    <h1 class="uppercase text-3xl font-semibold">Formulir Pendaftaran PPDB 2023/2024</h1>
+                </div>
+                <div x-init="$store.dataDiri.getDataDiri" x-show="$store.dataDiri.isExisted" class="mt-4 p-4 shadow-md bg-white">
+                    <p class="p-4 bg-[#D9EDF7] text-[#36829C]">
+                        <span>Kamu sudah mengisi formulir. </span>
+                        <span>Lihat data pendaftaranmu <a class="font-medium underline" href="/ppdb/hasil">di sini</a>.</span>
+                    </p>
+                </div>
+                @livewire('halaman-pendaftaran.formulir')
             {{-- </template> --}}
-        @livewire('login')
-        @livewire('registrasi')
+            @livewire('login')
+            @livewire('registrasi')
         </div>
     </section>
     
