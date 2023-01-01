5eb1f1381e78bfd12ee12f689c6fdbeccc5b53a6
fakhryfernanda <mff023@gmail.com>
2023-01-02 06:25:25 +0700

    (+) route /admin/sekolah

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\HasilPendaftaran;
-use App\Http\Controllers\AdminController;
 
-use App\Http\Livewire\Home;
+use App\Http\Livewire\Admin\DataSekolah;
+use App\Http\Livewire\Admin\DataPendaftar;
 use App\Http\Livewire\HalamanPendaftaran;
+use App\Http\Controllers\AdminController;
 use App\Http\Livewire\InformasiPendaftaran;
 
 Route::get('/', Home::class);
@@ -14,4 +16,6 @@ Route::get('/ppdb/formulir', HalamanPendaftaran::class);
 Route::get('/ppdb/informasi', InformasiPendaftaran::class);
 Route::get('/ppdb/hasil', HasilPendaftaran::class);
 
-Route::get('/admin', [AdminController::class, 'index']);
+// Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin', DataPendaftar::class);
+Route::get('/admin/sekolah', DataSekolah::class);
