4f29c1769b7d0f758a1f31c6b80b1f9599e77dbd
fakhryfernanda <mff023@gmail.com>
2023-01-02 06:26:09 +0700

    (+) komponen data sekolah

---
+++ app/Http/Livewire/Admin/DataSekolah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+
+class DataSekolah extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.data-sekolah')->layout('admin.index');
+    }
+}
---
+++ resources/views/livewire/admin/data-sekolah.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Halaman Sekolah</h1>
+</div>
